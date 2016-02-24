a2ad65bbee6f4978860cfe2e69e449e230aef328
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-02-24 18:26:51 +0100

    Scaffold created. Script file created.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'kmeans'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures kmeans'
+long_description 'Installs/Configures kmeans'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: kmeans
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
